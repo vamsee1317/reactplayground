b60198d248f98d5cca43c4dc9806f505756ebb02
GaletiVamsiKrishna <galetivamsikrishna@gmail.com>
2025-05-27 20:14:57 +0530

    ReactJs | Features | Installation using Vite | TailwindCSS SetUp

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-3xl bg-red-500 text-white text-center'>HelloWorld!!!!</h1>
+    </div>
+  )
+}
